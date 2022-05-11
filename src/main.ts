d5293bb183cc90c6ba9a31ec3438fac249339adb
Marcos <marcosvto1@gmail.com>
2022-05-10 21:22:08 -0300

    feat: initial config

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('WebMovies - API')
+    .setVersion('1.0')
+    .build();
+
+  const documentation = SwaggerModule.createDocument(app, documentationConfig);
+
+  SwaggerModule.setup('docs', app, documentation);
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
