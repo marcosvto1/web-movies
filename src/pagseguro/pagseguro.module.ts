b146531f8024afd3fc17bf7c82a67c2bea39636a
Marcos <marcosvto1@gmail.com>
2022-05-31 22:30:36 -0300

    fix: circular dependency

--- src/pagseguro/pagseguro.module.ts
+++ src/pagseguro/pagseguro.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PagSeguroService } from './pagseguro.service';
 import { PagseguroController } from './pagseguro.controller';
 import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [forwardRef(() => OrdersModule)],
   providers: [PagSeguroService],
   exports: [PagSeguroService],
   controllers: [PagseguroController],
